6c29c64108fa074e579b609892161e3635f1d19b
Alexander Metzner <alexander.metzner@gmail.com>
2022-06-10 08:42:27 +0200

    feature: disable http caching

--- main.go
+++ main.go
@@ -26,30 +26,51 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+	"path/filepath"
 	"syscall"
+	"time"
 
 	"github.com/halimath/kvlog"
 )
 
-const (
-	version = "0.1.0"
-)
-
 var (
+	version        = "0.1.0"
+	buildTimestamp = "now"
+	commit         = "local"
+
 	docRoot        = flag.String("doc-root", ".", "Document root")
-	httpAddress    = flag.String("http-address", ":8080", "Network address to bind to listen for incoming requests")
+	httpAddress    = flag.String("http-address", "localhost:8080", "Network address to bind to listen for incoming requests")
 	disableLogging = flag.Bool("no-log", false, "Disable logging")
+	cache          = flag.Bool("enable-caching", false, "Enable caching of unchanged files")
 )
 
 func main() {
-	fmt.Printf("mini-httpd v%s https://github.com/halimath/mini-httpd\nPublished under the Apache License version 2.\nServing at %s\n\n", version, *httpAddress)
+	flag.Parse()
+
+	fmt.Printf("mini-httpd v%s (%s; built %s)\nhttps://github.com/halimath/mini-httpd\nPublished under the Apache License version 2.\n\n", version, commit, buildTimestamp)
+
+	docRootAbs, err := filepath.Abs(*docRoot)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: failed to determine absolute path for %s: %s\n", os.Args[0], *docRoot, err)
+		os.Exit(1)
+	}
 
 	var handler http.Handler
-	handler = http.FileServer(http.Dir(*docRoot))
+	handler = http.FileServer(http.Dir(docRootAbs))
+
+	fmt.Printf("%20s: %s\n", "Docroot", docRootAbs)
+	fmt.Printf("%20s: %s\n", "Address", *httpAddress)
+
+	if !*cache {
+		handler = noCache(handler)
+		fmt.Printf("%20s: disabled\n", "Cache")
+	}
 
 	if !*disableLogging {
 		handler = kvlog.Middleware(kvlog.L, handler)
 	}
+
+	fmt.Println()
 
 	server := &http.Server{
 		Addr:    *httpAddress,
@@ -65,9 +86,39 @@ func main() {
 		server.Shutdown(context.Background())
 	}()
 
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 
 	if err != http.ErrServerClosed {
 		fmt.Fprintf(os.Stderr, "error running mini-httpd: %s", err)
 	}
+}
+
+var noCacheHeadersToAdd = map[string]string{
+	"Expires":         time.Unix(0, 0).Format(time.RFC1123),
+	"Cache-Control":   "no-cache, private, max-age=0",
+	"Pragma":          "no-cache",
+	"X-Accel-Expires": "0",
+}
+
+var noCacheHeadersToRemove = []string{
+	"ETag",
+	"If-Modified-Since",
+	"If-Match",
+	"If-None-Match",
+	"If-Range",
+	"If-Unmodified-Since",
+}
+
+func noCache(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		for _, h := range noCacheHeadersToRemove {
+			r.Header.Del(h)
+		}
+
+		for h, v := range noCacheHeadersToAdd {
+			w.Header().Set(h, v)
+		}
+
+		handler.ServeHTTP(w, r)
+	})
 }
